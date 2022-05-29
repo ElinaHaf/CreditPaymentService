d35a076343bd59666c60f94aac85ba2d9084e345
Элина Хафизова <elina6hafizova@yandex.ru>
2022-05-29 17:40:16 +0300

    Edit degree

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -3,60 +3,15 @@ public class CreditPaymentService {
         double creditService;                     //расчет ежемесячного платежа
         double credit = 1_000_000;                //сумма кредита
         double percent = 9.99;                    //годовая процентная ставка
-        double minPercentPerMonth;               //ежемесячная процентная ставка
+        double minPercentPerMonth;                //ежемесячная процентная ставка
         double annuity;
-
+        double month = loanTime * 12;                 //срок кредита/мес
 
         minPercentPerMonth = (percent / 100 / 12);
 
-        double minPercentPerMonth1;              //замена математической функции переменной
+        double degree = Math.pow((minPercentPerMonth + 1), month);  //возведенеие в степень
 
-        minPercentPerMonth1 = 1 + minPercentPerMonth;
-
-        double minPercentPerMonth112;            //замена математической функции переменной
-
-        minPercentPerMonth112 = (minPercentPerMonth1 * minPercentPerMonth1 *    //возведение в степень 12//
-                minPercentPerMonth1 * minPercentPerMonth1 * minPercentPerMonth1
-                * minPercentPerMonth1 * minPercentPerMonth1 * minPercentPerMonth1
-                * minPercentPerMonth1 * minPercentPerMonth1 * minPercentPerMonth1
-                * minPercentPerMonth1);
-
-
-        double minPercentPerMonth124 = (minPercentPerMonth1 * minPercentPerMonth1  //возведение в степень 24//
-                * minPercentPerMonth1 * minPercentPerMonth1 * minPercentPerMonth1
-                * minPercentPerMonth1 * minPercentPerMonth1 * minPercentPerMonth1
-                * minPercentPerMonth1 * minPercentPerMonth1 * minPercentPerMonth1
-                * minPercentPerMonth1 * minPercentPerMonth1 * minPercentPerMonth1
-                * minPercentPerMonth1 * minPercentPerMonth1 * minPercentPerMonth1
-                * minPercentPerMonth1 * minPercentPerMonth1 * minPercentPerMonth1
-                * minPercentPerMonth1 * minPercentPerMonth1 * minPercentPerMonth1
-                * minPercentPerMonth1);
-
-
-        double minPercentPerMonth136 = (minPercentPerMonth1 * minPercentPerMonth1 * //возведение в степень 36//
-                minPercentPerMonth1 * minPercentPerMonth1 * minPercentPerMonth1
-                * minPercentPerMonth1 * minPercentPerMonth1 * minPercentPerMonth1
-                * minPercentPerMonth1 * minPercentPerMonth1 * minPercentPerMonth1
-                * minPercentPerMonth1 * minPercentPerMonth1 * minPercentPerMonth1 *
-                minPercentPerMonth1 * minPercentPerMonth1 * minPercentPerMonth1
-                * minPercentPerMonth1 * minPercentPerMonth1 * minPercentPerMonth1
-                * minPercentPerMonth1 * minPercentPerMonth1 * minPercentPerMonth1
-                * minPercentPerMonth1 * minPercentPerMonth1 * minPercentPerMonth1 *
-                minPercentPerMonth1 * minPercentPerMonth1 * minPercentPerMonth1
-                * minPercentPerMonth1 * minPercentPerMonth1 * minPercentPerMonth1
-                * minPercentPerMonth1 * minPercentPerMonth1 * minPercentPerMonth1
-                * minPercentPerMonth1);
-
-        if (loanTime == 1) {
-            annuity = (minPercentPerMonth * minPercentPerMonth112) / (minPercentPerMonth112 - 1);
-
-        } else if (loanTime == 2) {
-            annuity = (minPercentPerMonth * minPercentPerMonth124) / (minPercentPerMonth124 - 1);
-
-        } else {
-            annuity = (minPercentPerMonth * minPercentPerMonth136) / (minPercentPerMonth136 - 1);
-        }
-
+        annuity = (minPercentPerMonth * degree) / (degree - 1);
 
         creditService = credit * annuity;
 
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
-        double loanTime = 1;   //срок кредита(год)
+        double loanTime = 2.5;   //срок кредита(год)
 
         double creditService = service.calculate(loanTime);
 
